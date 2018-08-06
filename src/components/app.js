39046d393b54931e3aa2bde8c622e75220f30894
Code <Code@batc.local>
2018-08-06 11:52:59 -0600

    Did the beggining of the button

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Picker from './picker';
+import Button from './button';
 
 export default class App extends Component {
   render() {
@@ -16,7 +17,10 @@ export default class App extends Component {
         <div className="grid__skew-light-box"></div>
       
         <Picker/>
+
+        { Button( `Generate Countdown`) }
       </div>
+      
     );
   }
 }
